7914f790b7dedf1723f36084fc530cbe9da38f9c
itsMounir <mounirtoo.22@gmail.com>
2024-02-28 14:01:44 +0300

    fix a bug

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
@@ -10,25 +11,35 @@ class Image extends Model
 {
     use HasFactory;
 
-    protected static function booted()
+    protected static function boot()
     {
         parent::boot();
 
-            static::retrieved(function ($image) {
-                $image->url = asset($image->url);
-            });
-    }
-
-    protected $guarded = [];
-
-    protected static function boot() {
-        parent::boot();
         static::deleting(function ($image) {
+            // dd($image->url);
+            // dd(static::where('url',$image->url)->exists());
             if (static::where('url',$image->url)->exists())
             {
                 Storage::disk('public')->delete($image->url);
             }
         });
+    }
+
+    protected $guarded = [];
+
+    protected function url(): Attribute
+    {
+        if (! (request()->route()->getName() == 'categories.destroy' || request()->route()->getName() == 'users.destroy' || request()->route()->getName() == 'products.destroy')) {
+            return Attribute::make(
+                get: fn (string $value) => asset($value),
+            );
+        }
+        else {
+            return Attribute::make(
+                get: fn (string $value) => $value,
+            );
+        }
+
     }
 
     public function imageable()
